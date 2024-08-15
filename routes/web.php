890c165628b2c77069a7c3325d97c62195a2a15f
WineSteward <miguel2001carpinteiro@hotmail.com>
2024-08-15 17:20:54 +0100

    correct database

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,31 @@
 <?php
 
+use App\Models\Job;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contacts', function () {
+    return view('contacts');
+});
+
+
+Route::get('/jobs', function () {
+    return view('jobs', [
+        'jobs' => Job::all()
+    ]);
+});
+
+Route::get('/jobs/{id}', function ($id)  {
+    
+    $job = Job::find($id);
+
+    return view('job', ['job' => $job]);
+});
