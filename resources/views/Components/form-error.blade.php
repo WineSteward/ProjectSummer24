4e2f5ddb19f4d0652fbdb4a8e29d8c9945a240fb
WineSteward <miguel2001carpinteiro@hotmail.com>
2024-09-10 17:44:11 +0100

    added more form components

--- resources/views/Components/form-error.blade.php
+++ resources/views/Components/form-error.blade.php
@@ -1,5 +1,5 @@
 @props(['name'])
 
-@error('name')
-<p class="mt-2 text-xd text-red-500 font-semibold">{{$message}}</p>
+@error($name)
+    <p class="mt-2 text-xd text-red-500 font-semibold">{{$message}}</p>
 @enderror
---
+++ resources/views/Components/form-field.blade.php
@@ -0,0 +1,1 @@
+<div {{$attributes->merge(['class' => 'sm:col-span-4'])}}>{{$slot}}</div>
